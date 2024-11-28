cd70d36dea284ed395521bbdcbd84f279b1cc158
KieranMueller <kieran98mueller@gmail.com>
2024-11-28 13:28:16 -0600

    new workflow

---
+++ src/app/views/home/home.component.spec.ts
@@ -0,0 +1,30 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing'
+
+import { HomeComponent } from './home.component'
+import { By } from '@angular/platform-browser'
+
+describe('HomeComponent', () => {
+    let component: HomeComponent
+    let fixture: ComponentFixture<HomeComponent>
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [HomeComponent]
+        })
+            .compileComponents()
+
+        fixture = TestBed.createComponent(HomeComponent)
+        component = fixture.componentInstance
+        fixture.detectChanges()
+    })
+
+    it('should create', () => {
+        expect(component).toBeTruthy()
+    })
+
+    it('should include button', () => {
+        const buttonEl = fixture.debugElement.query(By.css('#btn'))
+        expect(buttonEl).toBeTruthy()
+        expect(buttonEl.nativeElement.textContent).toContain('Click Me')
+    })
+})
